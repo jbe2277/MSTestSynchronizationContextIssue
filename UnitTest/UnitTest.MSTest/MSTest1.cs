8d2e442d277a0678595182e6541a16b3cca2fd86
jbe2277 <jbe2277@users.noreply.github.com>
2025-12-24 10:08:05 +0100

    Add repo solution

---
+++ UnitTest/UnitTest.MSTest/MSTest1.cs
@@ -0,0 +1,35 @@
+﻿using System.Waf.UnitTesting;
+
+namespace UnitTest.MSTest;
+
+[TestClass]
+public sealed class MSTest1
+{
+    public UnitTestSynchronizationContext Context { get; private set; } = null!;
+    
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        Context = UnitTestSynchronizationContext.Create();
+        Assert.AreSame(Context, SynchronizationContext.Current);
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        Assert.AreSame(Context, SynchronizationContext.Current);
+        Context.Dispose();
+    }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.AreSame(Context, SynchronizationContext.Current);
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        Assert.AreSame(Context, SynchronizationContext.Current);
+    }
+}
---
+++ UnitTest/UnitTest.XUnit/XTest1.cs
@@ -0,0 +1,32 @@
+﻿using System.Waf.UnitTesting;
+
+namespace UnitTest.XUnit;
+
+public sealed class XTest1 : IDisposable
+{
+    public UnitTestSynchronizationContext Context { get; private set; } = null!;
+
+    public XTest1()
+    {
+        Context = UnitTestSynchronizationContext.Create();
+        Assert.Same(Context, SynchronizationContext.Current);
+    }
+
+    public void Dispose()
+    {
+        Assert.Same(Context, SynchronizationContext.Current);
+        Context.Dispose();
+    }
+
+    [Fact]
+    public void TestMethod1()
+    {
+        Assert.Same(Context, SynchronizationContext.Current);
+    }
+
+    [Fact]
+    public void TestMethod2()
+    {
+        Assert.Same(Context, SynchronizationContext.Current);
+    }
+}
